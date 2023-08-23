1e961612dc527ce1a6812c01f007b00e3c038029
Pablo Andres Garcia <garciasjs@gmail.com>
2023-08-23 17:52:30 -0300

    ItemCount

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Navbar from './components/NavBar/NavBar';
 import ItemListContainer from './components/ItemListConteiner/ItemListContainer';
+import ItemCount from './components/ItemCount/ItemCount.jsx';
 import './index.css'
 
 const App = () => {
@@ -8,12 +9,12 @@ const App = () => {
     <div>
      
         <Navbar />
-   
         <ItemListContainer 
       greeting={<div className="content">
       <h1>Tienda de ropa</h1>
     </div>}
       />
+      <ItemCount initial={1} stock={10} onAdd={(quantity) => console.log('Cantidad agregada ', quantity)} /> 
     </div>
   );
 };
