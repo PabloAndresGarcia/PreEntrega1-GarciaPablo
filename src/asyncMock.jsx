6ae1d4e7c530742c39942d56ec01b1eb51432ec2
Pablo Andres Garcia <garciasjs@gmail.com>
2023-08-30 16:54:34 -0300

    css

--- src/asyncMock.jsx
+++ src/asyncMock.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
 const products = [
-    { id: 1, name: 'Remera', price: 1500, category: 'Remera', img: `https://cdn.pixabay.com/photo/2017/01/13/04/56/t-shirt-1976334_1280.png`, stock: 13, description: '' },
+    { id: 1, name: 'Remera', price: 1500, category: 'Remera', img: `https://cdn.pixabay.com/photo/2017/01/13/04/56/t-shirt-1976334_1280.png`, stock: 13, description: 'Remera blanca mangas cortas' },
     { id: 2, name: 'Zapatillas Nike', price: 2000, category: 'Zapatillas', img: 'https://cdn.pixabay.com/photo/2023/05/03/22/43/tennis-7968714_1280.png', stock: 5, description: '' },
     { id: 3, name: 'Producto 3', price: 1000, category: 'Remera', img: '', stock: 15, description: '' },
 ]
