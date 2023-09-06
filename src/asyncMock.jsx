3300817b77ef3590ddd531853e1f9c380ec9e71a
Pablo Andres Garcia <garciasjs@gmail.com>
2023-09-06 11:36:54 -0300

    Nav

--- src/asyncMock.jsx
+++ src/asyncMock.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 
 const products = [
-    { id: 1, name: 'Remera', price: 1500, category: 'Remera', img: `https://cdn.pixabay.com/photo/2017/01/13/04/56/t-shirt-1976334_1280.png`, stock: 13, description: 'Remera blanca mangas cortas' },
-    { id: 2, name: 'Zapatillas Nike', price: 2000, category: 'Zapatillas', img: 'https://cdn.pixabay.com/photo/2023/05/03/22/43/tennis-7968714_1280.png', stock: 5, description: '' },
-    { id: 3, name: 'Producto 3', price: 1000, category: 'Pantalon', img: '', stock: 15, description: '' },
+    { id: 1, name: 'Remera', price: 1500, category: 'remera', img: `https://cdn.pixabay.com/photo/2017/01/13/04/56/t-shirt-1976334_1280.png`, stock: 13, description: 'Remera blanca mangas cortas' },
+    { id: 2, name: 'Zapatillas Nike', price: 2000, category: 'zapatillas', img: 'https://cdn.pixabay.com/photo/2023/05/03/22/43/tennis-7968714_1280.png', stock: 5, description: '' },
+    { id: 3, name: 'Pantalon', price: 1000, category: 'pantalon', img: '', stock: 15, description: '' },
 ]
 
 export const getProducts = () => {
@@ -17,7 +17,7 @@ export const getProducts = () => {
 export const getProductById = (productId) => {
     return new Promise((resolve) => {
         setTimeout(() => {
-            resolve(products.filter(prod => prod.id === parseInt(productId)))
+            resolve(products.find(prod => prod.id === parseInt(productId)))
         }, 500)
     })
 }
@@ -25,7 +25,7 @@ export const getProductById = (productId) => {
 export const getProductsByCategory = (productCat) => {
     return new Promise((resolve) => {
         setTimeout(() => {
-            resolve(products.filter(prod => prod.category === parseInt(productCat)))
+            resolve(products.filter(prod => prod.category === (productCat)))
         }, 500)
     })
 }
